09006ebd7c6e74bc37470f86dac8f369da8e6a80
Abdou Ett <abdou@macbook-pro-de-abdou.local>
2017-01-07 12:05:20 +0000

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Abdou Ett on 07/01/2017.
+//  Copyright © 2017 Abdou Ett. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
